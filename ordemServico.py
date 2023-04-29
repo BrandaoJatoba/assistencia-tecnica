994dadb2e4ac4d8abda2f0ea291e5fa889cc8d6d
brandaojatoba <brandaojatoba@gmail.com>
2023-04-29 18:50:33 -0300

    "Update: base classes. Create: Enum for status"

---
+++ ordemServico.py
@@ -0,0 +1,9 @@
+class OrdemServico:
+    
+    def __init__(self, id:int, client, tecnico, status) -> None:
+        self.__int = int
+        self.__client = client
+        self.__tecnico = tecnico
+        self.__status = status
+        self.__log = []
+        pass
