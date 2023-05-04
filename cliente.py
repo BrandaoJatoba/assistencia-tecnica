b3fb8e44f90db353056143952a975cee23e9f133
João Jatobá <brandaojatoba@gmail.com>
2023-05-04 00:25:31 -0300

    update: concrete client class implemented

---
+++ cliente.py
@@ -0,0 +1,43 @@
+class Cliente:
+
+    def __init__(self, nome, cpf, telefone, equipamento) -> None:
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__telefone = telefone
+        self.__equipamento = [equipamento]
+        pass
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def cpf(self):
+        return self.__cpf
+    
+    @property
+    def telefone(self):
+        return self.__telefone
+    
+    @property
+    def equipamento(self) -> list:
+        return self.__equipamento
+    
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+        pass
+
+    @cpf.setter
+    def cpf(self, cpf):
+        self.__cpf = cpf
+        pass
+
+    @telefone.setter
+    def telefone(self, telefone):
+        self.__telefone = telefone
+        pass
+
+    def addEquipamento(self, equipamentoNovo):
+        self.__equipamento.append(equipamentoNovo)
+        pass
